e13049e81724fc19f11ed63cba40cefabf86501b
tianme <15978852400@163.com>
2018-09-20 14:25:28 +0800

    refactor: bulid

--- dist/common/HttpHelper.js
+++ dist/common/HttpHelper.js
@@ -1,18 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const requestHeader_1 = require("../common/requestHeader");
+const caches_1 = require("../cache/caches");
 const utils_1 = require("../common/utils");
+const caches = caches_1.default.getInstance();
 const request = require('superagent');
 class HttpHelper {
     static post(url, data, tokenKey) {
         const dataStr = JSON.stringify(data);
+        const requestHeader = caches.getRequestHeader();
         return request.post(url)
-            .set('product_id', requestHeader_1.default.productId)
-            .set('u_id', requestHeader_1.default.uid)
-            .set('feed_id', requestHeader_1.default.feedId)
-            .set('local_token', utils_1.default.MD5(dataStr + tokenKey))
-            .set('Content-Type', requestHeader_1.default.contentType)
+            .set('product_id', requestHeader.productId)
+            .set('u_id', requestHeader.uid)
+            .set('feed_id', requestHeader.feedId)
+            .set('local_token', utils_1.default.md5(dataStr + tokenKey))
+            .set('Content-Type', requestHeader.contentType)
             .send(dataStr);
     }
 }
 exports.default = HttpHelper;
+//# sourceMappingURL=HttpHelper.js.map
--- dist/common/utils.js
+++ dist/common/utils.js
@@ -3,12 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const crypto = require("crypto");
 const fs = require("mz/fs");
 const Log4js = require("log4js");
-const requestHeader_1 = require("../common/requestHeader");
 const Path = require("path");
 const logger = Log4js.getLogger('utils');
-class utils {
-    static MD5(str) {
-        return crypto.createHash('md5').update(str).digest('hex');
+class Utils {
+    static md5(str) {
+        return crypto
+            .createHash('md5')
+            .update(str)
+            .digest('hex');
     }
     static readWebconfig(path) {
         let currentConfig = null;
@@ -25,21 +27,24 @@ class utils {
             workConfig = JSON.parse(configStr);
         }
         const config = Object.assign({}, currentConfig, workConfig);
-        return utils.convertToken(config);
+        return config;
     }
     static convertToken(config) {
-        const authenticationTokenKey = config["authenticationTokenKey"];
-        const auth_token_key = authenticationTokenKey.split('_');
-        if (!utils.checkAuthenticationTokenKey(auth_token_key)) {
-            logger.error('authenticationTokenKey格式错误');
+        const authenticationTokenKey = config['authenticationTokenKey'];
+        const authTokenKey = authenticationTokenKey.split('_');
+        if (!Utils.checkAuthenticationTokenKey(authTokenKey)) {
+            logger.error('authenticationTokenKey 格式错误');
         }
-        const feedId = auth_token_key[2];
-        const productId = auth_token_key[1];
-        const uid = auth_token_key[0];
-        requestHeader_1.default.feedId = feedId;
-        requestHeader_1.default.productId = productId;
-        requestHeader_1.default.uid = uid;
-        return Object.assign({}, config, { feedId, productId, uid });
+        const feedId = authTokenKey[2];
+        const productId = authTokenKey[1];
+        const uid = authTokenKey[0];
+        const requestHeader = {
+            feedId,
+            productId,
+            uid,
+            contentType: 'application/json',
+        };
+        return requestHeader;
     }
     static checkAuthenticationTokenKey(token) {
         if (token.length !== 4) {
@@ -48,4 +53,5 @@ class utils {
         return true;
     }
 }
-exports.default = utils;
+exports.default = Utils;
+//# sourceMappingURL=utils.js.map
